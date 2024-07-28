f39d7ae244d5b2da34120c0e2ac7f4d4540a0a03
Rishabh Jain <hello@rishabhjain.dev>
2024-07-28 22:46:47 +0530

    chore: Update npm dependency to latest stable version

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,25 +1,39 @@
-import { MenuIcon, SearchIcon } from "lucide-react";
-import { Button } from "./ui/button";
-import { Sheet, SheetTrigger, SheetContent } from "@/components/ui/sheet";
+"use client";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { MenuIcon } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { useRouter } from "next/navigation";
 
 export const Header = () => {
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+
+  const openLink = (e: React.MouseEvent<HTMLAnchorElement>) => {
+    e.preventDefault();
+    e.stopPropagation();
+    setOpen(false);
+    router.push(e.currentTarget.href);
+  };
+
   return (
     <header className="flex items-center justify-between border-b border-border bg-foreground p-4 md:px-6">
       <div className="flex items-center gap-2">
         <Image width={40} height={40} src="/icons/RJ Logo Small.webp" alt="Logo" />
-        <span className="text-2xl font-bold">Rishabh Jain</span>
+        <span className="hidden text-2xl font-bold md:inline-block">Rishabh Jain</span>
       </div>
       <nav className="hidden gap-6 md:flex">
-        <a href="#about" className="hover:text-gray-400">
+        <Link onClick={openLink} href="#about" className="hover:text-gray-400">
           About
-        </a>
-        <a href="#portfolio" className="hover:text-gray-400">
+        </Link>
+        <Link onClick={openLink} href="#portfolio" className="hover:text-gray-400">
           Portfolio
-        </a>
-        <a href="#contact" className="hover:text-gray-400">
+        </Link>
+        <Link onClick={openLink} href="#contact" className="hover:text-gray-400">
           Contact
-        </a>
+        </Link>
       </nav>
       {/* <div className="relative">
         <input
@@ -29,7 +43,7 @@ export const Header = () => {
         />
         <SearchIcon className="absolute left-2.5 top-1/2 h-4 w-4 -translate-y-1/2 text-gray-400" />
       </div> */}
-      <Sheet>
+      <Sheet open={open} onOpenChange={setOpen}>
         <SheetTrigger asChild>
           <Button variant="outline" size="icon" className="md:hidden">
             <MenuIcon className="h-6 w-6" />
@@ -38,15 +52,15 @@ export const Header = () => {
         </SheetTrigger>
         <SheetContent side="left" className="text-white md:hidden">
           <nav className="grid gap-4 p-4">
-            <a href="#" className="hover:text-gray-400">
+            <Link onClick={openLink} href="#about" className="hover:text-gray-400">
               About
-            </a>
-            <a href="#" className="hover:text-gray-400">
+            </Link>
+            <Link onClick={openLink} href="#portfolio" className="hover:text-gray-400">
               Portfolio
-            </a>
-            <a href="#" className="hover:text-gray-400">
+            </Link>
+            <Link onClick={openLink} href="#contact" className="hover:text-gray-400">
               Contact
-            </a>
+            </Link>
           </nav>
         </SheetContent>
       </Sheet>
--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -6,6 +6,7 @@ import { X } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { ComponentPropsWithoutRef, ElementRef, forwardRef, HTMLAttributes } from "react";
+import { FocusScope } from "@radix-ui/react-focus-scope";
 
 const Sheet = Root;
 
@@ -54,13 +55,15 @@ const SheetContent = forwardRef<ElementRef<typeof Content>, SheetContentProps>(
   ({ side = "right", className, children, ...props }, ref) => (
     <SheetPortal>
       <SheetOverlay />
-      <Content ref={ref} className={cn(sheetVariants({ side }), className)} {...props}>
-        {children}
-        <Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-          <X className="h-4 w-4" />
-          <span className="sr-only">Close</span>
-        </Close>
-      </Content>
+      <FocusScope trapped={false}>
+        <Content ref={ref} className={cn(sheetVariants({ side }), className)} {...props}>
+          {children}
+          <Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+            <X className="h-4 w-4" />
+            <span className="sr-only">Close</span>
+          </Close>
+        </Content>
+      </FocusScope>
     </SheetPortal>
   ),
 );
